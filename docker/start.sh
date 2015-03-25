0a80c909bfcd7b770ee2ca0df9c5fb5782630095
Sebastian Lauber <sebastian.lauber@dasscrumteam.com>
2015-03-25 15:50:54 +0100

    Updated Crawcial Dockerfile to official repositories

--- docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /data/crawcial && git pull
-if [ ! -e /data/crawcial/store.keystore ]; then
-        mvn keytool:generateKeyPair
-fi
-mvn compiler:compile resources:resources jetty:run
